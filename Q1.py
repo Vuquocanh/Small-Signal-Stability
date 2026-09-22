4df90866dba7df5a21e47f3bfbac2bc83c41beee
Vuquocanh <108507484+Vuquocanh@users.noreply.github.com>
2026-09-22 22:45:58 +0200

    Add AVR and PSS considerations for eigenvalue calculations and participation matrix export

--- Q1.py
+++ Q1.py
@@ -46,7 +46,7 @@ names = sys_data['names_q1b']
 latex_names = sys_data['latex_names_q1b']
 
 print("------------------------------------------------------------------------------")
-print("Q1.2.1: Calculating eigenvalues of the system matrix and frequency, damping")
+print("Q1.2.1: Considering AVR, calculating eigenvalues of the system matrix and frequency, damping")
 eigen_vals, Phi = np.linalg.eig(matrix_A)
 for lam in eigen_vals:
     sigma = lam.real
@@ -69,3 +69,37 @@ for i in range(A_size[0]):
         
 pmw.latex_P_matrix(P, names, False, result_path("Results_Q1_2", "P_matrix_Q1_2.tex"), A_size[0], 0.05)
 pmw.excel_P_matrix(P, names, False, result_path("Results_Q1_2", "P_matrix_Q1_2.xls"), 0.05)
+
+#------------------------------------------------------------------------------
+#Q1.3.1: Effect of AVR+PSS, Calculating eignevalues of the system matrix and 
+# frequency, damping of the oscillation modes
+#------------------------------------------------------------------------------
+sys_data = sio.loadmat('./Assignment_data/system_q1c.mat', squeeze_me=True)
+matrix_A = sys_data['A_q1c']
+names = sys_data['names_q1c']
+latex_names = sys_data['latex_names_q1c']
+
+print("------------------------------------------------------------------------------")
+print("Q1.3.1: Considering PSS, calculating eigenvalues of the system matrix and frequency, damping")
+eigen_vals, Phi = np.linalg.eig(matrix_A)
+for lam in eigen_vals:
+    sigma = lam.real
+    omega = lam.imag
+    if abs(omega) > 10e-6:
+        freq = abs(omega)/(2*math.pi)
+        damp = -sigma/(np.sqrt(sigma**2 + omega**2))
+        print(f"Eigenvalue: {lam:.4f}, Frequency: {freq:.4f} Hz, Damping: {damp:.4f}")
+    else:
+        print(f"Eigenvalue: {lam:.4f}")
+print("------------------------------------------------------------------------------")
+
+#Participation Matrix P_ij = phi_ij * psi_ji
+A_size = matrix_A.shape
+P = np.zeros(matrix_A.shape, dtype = complex)
+Psi = np.linalg.inv(Phi)
+for i in range(A_size[0]):
+    for j in range(A_size[1]):
+        P[i, j] = Phi[i, j] * Psi[j, i]
+        
+pmw.latex_P_matrix(P, names, False, result_path("Results_Q1_3", "P_matrix_Q1_3.tex"), A_size[0], 0.05)
+pmw.excel_P_matrix(P, names, False, result_path("Results_Q1_3", "P_matrix_Q1_3.xls"), 0.05)
